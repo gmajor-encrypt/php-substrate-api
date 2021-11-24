5a0a1a14a0ca8bb87a1ec6af2f0f79ca3cd36a0b
gmajor <gmajorencrypt@gmail.com>
2021-11-24 16:42:24 +0800

    Update StorageKey.php

--- src/Rpc/StorageKey.php
+++ src/Rpc/StorageKey.php
@@ -2,7 +2,88 @@
 
 namespace Rpc;
 
+use Rpc\Hasher\Hasher;
+
 class StorageKey
 {
+    public string $scaleType;
+
+    public string $encodeKey;
+
+    /**
+     * StorageKey encode
+     *
+     * @param string $moduleName
+     * @param string $storageName
+     * @param array $metadata
+     * @param array $args
+     * @return StorageKey
+     *
+     * @throws \SodiumException
+     */
+    public static function encode (string $moduleName, string $storageName, array $metadata, array $args = array()): StorageKey
+    {
+        $storageName = ucfirst($storageName);
+
+        $storageItem = array();
+
+        foreach ($metadata["modules"] as $v) {
+            if (strcasecmp($v["name"], $moduleName)) {
+                foreach ($v["storage"]["items"] as $item) {
+                    if (strcasecmp($item["prefix"], $storageName)) {
+                        $storageItem = $item;
+                    }
+                }
+            }
+        }
+        if (count($storageItem) == 0) {
+            throw new \InvalidArgumentException(sprintf("invalid storage prefix %s", $storageName));
+        }
+
+        $valueType = "";
+        $hashers = [];
+
+        switch ($storageItem["type"]["origin"]) {
+            case "MapType":
+                $valueType = $storageItem["type"]["map_type"]["value"];
+                array_push($hashers, $storageItem["type"]["map_type"]["hasher"]);
+                break;
+            case "Map":
+                $valueType = $storageItem["type"]["MapType"]["values"];
+                $hashers = array_merge($hashers, $storageItem["type"]["MapType"]["hashers"]);
+                break;
+            case "DoubleMapType":
+                $valueType = $storageItem["type"]["DoubleMapType"]["value"];
+                $hashers = array_merge($hashers, [$storageItem["type"]["MapType"]["hashers"], $storageItem["type"]["DoubleMapType"]["key2Hasher"]]);
+                break;
+            case "PlainType":
+                $valueType = $storageItem["type"]["plain_type"];
+                array_push($hashers, "Twox64Concat");
+                break;
+            case "NMap":
+                $valueType = $storageItem["type"]["NMap"]["value"];
+                $hashers = array_merge($hashers, $storageItem["type"]["NMapType"]["hashers"]);
+                break;
+        }
+        if (count($args) != count($hashers)) {
+            throw new \InvalidArgumentException(sprintf("invalid args, expect %d, actual %d ", count($hashers), count($args)));
+        }
+
+        $hash = new Hasher();
+        $encodeKey = $hash->ByHasherName($moduleName, "Twox128") . $hash->ByHasherName($storageName, "Twox128");
+
+        foreach ($args as $index => $arg) {
+            $encodeKey = $encodeKey . $hash->ByHasherName($arg, $hashers[$index]);
+        }
+
+        return new StorageKey($valueType, $encodeKey);
+    }
+
+    public function __construct (string $scaleType, string $encodeKey)
+    {
+        $this->encodeKey = $encodeKey;
+        $this->scaleType = $scaleType;
+
+    }
 
 }
